27ab910c3a3445e11c96e31f3a0b0541c154160f
Jacob <akintolajacob75@gmail.com>
2026-04-17 12:08:15 +0100

    feat: create a function that returns user's input as a dictionary

--- script.js
+++ script.js
@@ -18,14 +18,24 @@ const showInfoModal = (title, message) => {
     infoModalElement.style.display = 'flex'
 }
 
+function collectUserInput(inputFieldsId) {
+    const userInputDictionary = {}
+    
+    for (let id = 0; id < inputFieldsId.length; id++) {
+        const dictionaryKey = `user${inputFieldsId[id]}`
+        userInputDictionary[dictionaryKey] = document.getElementById(`${inputFieldsId[id]}`).value.trim();
+    }
+
+    return userInputDictionary;
+}
+
 const addTransaction = () => {
     // Collect user's input
-    userTransactionName = document.getElementById('transactionName').value.trim();
-    userTransactionAmount = document.getElementById('transactionAmount').value.trim();
-    userTransactionType = document.getElementById('transactionType').value.trim();
+    let userInput = collectUserInput(["transactionName", "transactionAmount", "transactionType"])
 
     // Input validation
-    if (userTransactionName === '' || userTransactionAmount === '') {
-        showInfoModal("Missing Details", "Please fill in all input fields before adding a transaction")
-    }
+    // if (userTransactionName === '' || userTransactionAmount === '') {
+    //     showInfoModal("Missing Details", "Please fill in all input fields before adding a transaction");
+    //     return;
+    // }
 }
