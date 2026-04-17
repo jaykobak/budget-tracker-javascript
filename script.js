dfd64b0d0a66951c0d01fdfe30f358c5bc5b0692
Jacob <akintolajacob75@gmail.com>
2026-04-17 07:36:03 +0100

    feat: collect user's input

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const allTransactions = []
+
 // Function to close any modal
 const closeModal = () => {
     infoModalElement = document.getElementById('infoModal')
@@ -14,6 +16,13 @@ const showInfoModal = (title, message) => {
 
     infoModalElement = document.getElementById('infoModal')
     infoModalElement.style.display = 'flex'
+}
+
+const addTransaction = () => {
+    // Collect user's input
+    userTransactionName = document.getElementById('transactionName').value;
+    userTransactionAmount = document.getElementById('transactionAmount').value;
+    userTransactionType = document.getElementById('transactionType').value;
 }
 
 // showInfoModal("Missing Details", "Please fill in all input fields before adding a transaction")
